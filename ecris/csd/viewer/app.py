aa7f54bc327f66e2216603728740efeccad395c9
Jessica Rehak <rehak@lbl.gov>
2025-04-16 13:16:19 -0700

    overhauled elements selector

--- ecris/csd/viewer/app.py
+++ ecris/csd/viewer/app.py
@@ -38,9 +38,9 @@ class CSDViewer(tk.Tk):
         self.plot.set_element_indicators(self.element_buttons.element_visibility)
 
         self.plot.pack(side='left', fill='both', expand=True)
-        self.file_list.pack()
+        self.file_list.pack(padx=10, pady=10)
         self.file_list_controls.pack()
-        self.element_buttons.pack(fill="both")
+        self.element_buttons.pack(fill="both", padx=10, pady=10)
         self.controls.pack()
         # self.file_list_controls.grid(row=1, column=1, padx=self.pad, pady=self.pad, sticky="N")
         # self.file_list.grid(row=0, column =1, padx=self.pad, pady=self.pad, sticky="N") 
--- ecris/csd/viewer/gui/elements.py
+++ ecris/csd/viewer/gui/elements.py
@@ -8,39 +8,46 @@ class ElementButtons(tk.Frame):
                  persistent_elements: List[Element], 
                  variable_elements: List[Element], 
                  *args, **kwargs):
-        super().__init__(owner, relief=tk.SUNKEN, highlightbackground="gray", highlightthickness=2, *args, **kwargs)
+        super().__init__(owner, relief=tk.GROOVE, 
+                         padx=20, pady=20, borderwidth=5, *args, **kwargs)
         self._persistent_elements = persistent_elements
         self._variable_elements = variable_elements
         self.element_visibility = {
             e: tk.BooleanVar(value=False) for e in persistent_elements + variable_elements
         }
         self._plot = plot
+        self._font = "TkDefaultFont"
+        self._title_font = (self._font, 14)
+        self._subtitle_font = (self._font, 12)
+        
         self.create_widgets()
 
     def create_widgets(self):
-        lbTitle = tk.Label(self, text='Element M/Q indicators')
+        lbTitle = tk.Label(self, text='Element M/Q indicators', font=self._title_font)
         lbTitle.grid(sticky='N', column=0, row=0, columnspan=2)
+        lbOptions = tk.Label(self, text='Display options', font=self._subtitle_font)
+        lbOptions.grid(column=0, row=1, sticky='W')
         button = tk.Checkbutton(self, text='Show lines',
                                 onvalue=True, offvalue=False,
                                 variable=self._plot.draw_element_lines,
                                 command=self._plot.update)
-        button.grid(sticky='S', column=0, row=1)
-        lbPersistent = tk.Label(self, text='Persistent')
-        lbPersistent.grid(sticky='N', column=0, row=2)
+        button.grid(sticky='NW', column=0, row=2)
+        lbPersistent = tk.Label(self, text='Persistent', font=self._subtitle_font)
+        lbPersistent.grid(column=0, row=3, sticky='W')
         for i, element in enumerate(sorted(self._persistent_elements, key=lambda e: e.atomic_number)):
             text = f"{element.symbol}-{element.atomic_weight}"
             button = tk.Checkbutton(self, text=text,
                                     onvalue=True, offvalue=False,
                                     variable=self.element_visibility[element],
                                     command=self._plot.update)
-            button.grid(sticky='NW', row=3+i, column=0)
+            button.grid(sticky='NW', row=4+i, column=0)
             
-        lbVariable = tk.Label(self, text='Variable')
-        lbVariable.grid(sticky='N', column=1, row = 2)
+        lbVariable = tk.Label(self, text='Variable', font=self._subtitle_font)
+        lbVariable.grid(column=1, row = 3, sticky='W')
         for i, element in enumerate(sorted(self._variable_elements, key=lambda e: e.atomic_number)):
             text = f"{element.symbol}-{element.atomic_weight}"
             button = tk.Checkbutton(self, text=text,
                                     onvalue=True, offvalue=False,
                                     variable=self.element_visibility[element],
                                     command=self._plot.update)
-            button.grid(sticky='NW', column=1, row=3+i)
+            button.grid(sticky='NW', column=1, row=4+i)
