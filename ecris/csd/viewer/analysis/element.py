b65ec363e9a20a3e3e862ecfe9720047f757566d
Jessica Rehak <rehak@lbl.gov>
2025-04-18 10:30:41 -0700

    corrected titanium atomic mass to 50

--- ecris/csd/viewer/analysis/element.py
+++ ecris/csd/viewer/analysis/element.py
@@ -20,6 +20,6 @@ VARIABLE_ELEMENTS = [
     Element(name=n, symbol=s, atomic_weight=a, atomic_number=z)
     for n,s,a,z in [
         ("Vanadium", "V", 51, 23),
-        ("Titanium", "Ti", 51, 22)
+        ("Titanium", "Ti", 50, 22)
     ]
 ]
