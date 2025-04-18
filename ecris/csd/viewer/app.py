befd787777de759c9a01265ae6ba53c245fad3ac
Jessica Rehak <rehak@lbl.gov>
2025-04-18 12:22:27 -0700

    updated version to 1.0.1

--- ecris/csd/viewer/app.py
+++ ecris/csd/viewer/app.py
@@ -8,7 +8,7 @@ from ecris.csd.viewer.gui.elements import ElementButtons
 from .gui import FileList, PlotControls, Plot, FileListControls
 from .analysis.element import PERSISTANT_ELEMENTS, VARIABLE_ELEMENTS
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 matplotlib.rc('font', size=14)
 
