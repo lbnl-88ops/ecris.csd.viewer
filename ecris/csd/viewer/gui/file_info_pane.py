b5c6be1c77f296b61c92ee9800e5cbaf80435d51
Jessica Rehak <rehak@lbl.gov>
2025-05-13 10:28:09 -0700

    huge restructuring of info pane data handling

--- ecris/csd/viewer/gui/file_info_pane.py
+++ ecris/csd/viewer/gui/file_info_pane.py
@@ -1,7 +1,8 @@
 import logging
+from dataclasses import dataclass
 import tkinter as tk
 import ttkbootstrap as ttk
-from typing import Dict
+from typing import Dict, List, Optional
 
 from ecris.csd.analysis import CSD
 from ecris.csd.viewer.files import CSDFile
@@ -9,88 +10,94 @@ from ecris.csd.viewer.files import CSDFile
 _FONT = "TkDefaultFont"
 _TITLE_FONT = (_FONT, 14)
 _SUBTITLE_FONT = (_FONT, 12)
+_COLUMN_FONT = (_FONT, 10)
 
-_INFO_BLOCKS = [
-    "Vacuum (torr)", 
-    "Superconductors (A)",
-    "HV Voltage (V)",
-    "HV Current (mA)",
-    "Glaser (A)",
+
+class CSDInfoRow:
+    def __init__(self, csd_settings: str | List[str], formats: str | List[str], info_label: Optional[str] = ''):
+        if isinstance(csd_settings, str):
+            if info_label is None:
+                info_label = csd_settings
+            csd_settings = [csd_settings]
+        if isinstance(formats, str):
+            formats = [formats]
+        if len(formats) != len(csd_settings):
+            raise RuntimeError('Length of formats and csd settings must be the same')
+        self.csd_settings = csd_settings
+        self.formats = formats
+        self.info_label = info_label
+
+class CSDInfoBlock(ttk.Label):
+    def __init__(self, owner: ttk.Frame, csd_setting: str, format: str):
+        super().__init__(owner, relief=ttk.RAISED, borderwidth=2
+                      
+                         )
+        self.csd_setting = csd_setting
+        self.format = format
+    
+    def update(self, csd: Optional[CSD] = None):
+        text = 'No CSD Data'
+        if csd is not None:
+            text = f'{csd.settings[self.csd_setting]:{self.format}}'
+        self.config(text=text)
+
+_FRAMES = [
+    'Vacuum',
+    ]
+_COLUMNS = [
+    ['(torr)']
+    ]
+_INFO_ROWS = [
+    [
+        CSDInfoRow('inj_mbar', '.1e', 'Injection'),
+        CSDInfoRow('ext_mbar', '.1e', 'Extraction'),
+        CSDInfoRow('bl_mig2_torr', '.1e', 'Beam line'),
+    ]
 ]
 
-_INFO_BLOCKS_FORMATS = [
-    '.1e',
-    '6.2f',
-    '.2f',
-    '.3e',
-    '.1f'
-]
-
-_INFO_BLOCKS_LABELS_AND_VALUES = [
-    {'Injection': 'inj_mbar',
-     'Extraction': 'ext_mbar',
-     'Beam line': 'bl_mig2_torr'},
-     ['inj_i', 'ext_i', 'mid_i', 'sext_i'],
-     {'Extraction': 'extraction_v',
-      'Puller': 'puller_v',
-      'Biased Disk': 'bias_v'},
-     {'Extraction': 'extraction_i',
-      'Puller': 'puller_i',
-      'Biased Disk': 'bias_i'},
-      ['glaser_1']
-]
 
 class CSDInfoFrame(ttk.Frame):
-    def __init__(self, 
-                 owner, 
-                 frame_title: str,
-                 format: str,
-                 labels_and_values: Dict[str, str],
-                 *args, 
-                 **kwargs):
-        super().__init__(owner, *args, **kwargs)
+    def __init__(self, owner, frame_title: str, 
+                 csd_info_rows: CSDInfoRow | List[CSDInfoRow], 
+                 column_titles: List[str] | None):
+        super().__init__(owner, relief=ttk.RAISED)
         self.frame_title = frame_title
-        self.format = format
-        self.labels_and_values = labels_and_values
-        self.labels: Dict = {}
+        if not isinstance(csd_info_rows, list):
+            csd_info_rows = [csd_info_rows]
+        if column_titles is None:
+            column_titles = []
+        self.info_rows = csd_info_rows
+        self.column_titles = column_titles
+        self.info_blocks = []
         self.create_widgets()
+        self.update_csd_info()
 
     def create_widgets(self):
-        ttk.Label(self, text=self.frame_title, font=_SUBTITLE_FONT).pack(side='top')
-        if isinstance(self.labels_and_values, dict):
-            labels = self.labels_and_values.keys()
-        else:
-            labels = self.labels_and_values
-        for label in labels:
-            frInfo = ttk.Frame(self)
-            ttk.Label(frInfo, text=label).pack(side='left')
-            self.labels[label] = ttk.Label(frInfo)
-            self.labels[label].pack(side='right')
-            frInfo.pack(fill='x')
-        self.update_data_labels(None)
+        grid_width = 1 + max(len(row.csd_settings) for row in self.info_rows)
+        row_start = 1 if self.column_titles is None else 2
+        ttk.Label(self, text=self.frame_title, font=_SUBTITLE_FONT,
+                    relief=ttk.RAISED, borderwidth=2,
+                  ).grid(row=0, column=0,
+                                                                         columnspan=grid_width)
+        for i, title in enumerate(self.column_titles):
+            ttk.Label(self, text=title, font=_COLUMN_FONT, 
+                    relief=ttk.RAISED, borderwidth=2,
+                      ).grid(row=1, column=1 + i)
 
-    def update_data_labels(self, csd: CSD | None):
-        if isinstance(self.labels_and_values, dict):
-            labels = self.labels_and_values.keys()
-            values = self.labels_and_values.values()
-        elif isinstance(self.labels_and_values, list):
-            labels = self.labels_and_values
-            values = self.labels_and_values
-        else:
-            raise ValueError('Incorrect form')
+        for i, row in enumerate(self.info_rows):
+            n_row = row_start + i
+            ttk.Label(self, text=row.info_label,
+                    relief=ttk.RAISED, borderwidth=2,
+                      ).grid(row=n_row, column=0, 
+                                                      sticky='w')
+            for j, setting in enumerate(row.csd_settings):
+                self.info_blocks.append(CSDInfoBlock(self, setting, row.formats[j]))
+                self.info_blocks[-1].grid(row=n_row, column = j + 1, 
+                                          sticky='ew')
 
-        for label, value in zip(labels, values):
-            if csd is None:
-                text = 'No CSD data'
-            else:
-                text = f'{csd.settings[value]:{self.format}}'
-            self.labels[label].config(text=text)
-
-class CSDInfo:
-    def __init__(self, csd: CSD):
-        self._csd = csd
-        self.data_points = len(csd.time)
-        self.extraction_voltage = csd.extraction_voltage
+    def update_csd_info(self, csd: CSD | None = None):
+        for info_block in self.info_blocks:
+            info_block.update(csd)
 
 class FileInfoPane(ttk.Frame):
     def __init__(self, owner, *args, **kwargs):
@@ -101,7 +108,7 @@ class FileInfoPane(ttk.Frame):
         self._subtitle_font = (self._font, 12)
         self._info_widgets = []
         self._file_info = {}
-        self._csd_info_frames = []
+        self._csd_info_frames: List[CSDInfoFrame] = []
         self.visible = tk.BooleanVar(value=False)
         self.visible.trace_add('write', self.set_visible)
 
@@ -133,8 +140,9 @@ class FileInfoPane(ttk.Frame):
         
         tk.Label(self.canvas, text='CSD Info', font=self._title_font).pack()
 
-        for title, fmt, data in zip(_INFO_BLOCKS, _INFO_BLOCKS_FORMATS, _INFO_BLOCKS_LABELS_AND_VALUES):
-            self._csd_info_frames.append(CSDInfoFrame(self.canvas, title, fmt, data))
+        for title, csd_info_rows, column_titles in zip(_FRAMES, _INFO_ROWS, _COLUMNS):
+            self._csd_info_frames.append(CSDInfoFrame(self.canvas, 
+                                                      title, csd_info_rows, column_titles))
             self._csd_info_frames[-1].pack(fill='x')
 
         self.scrollbar = ttk.Scrollbar(self, orient='vertical')
@@ -151,15 +159,15 @@ class FileInfoPane(ttk.Frame):
             for attribute, variable in self._file_info.items():
                 variable.set('No file selected')
             for frame in self._csd_info_frames:
-                frame.update_data_labels(None)
+                frame.update_csd_info()
         elif not file.valid:
             for attribute, variable in self._file_info.items():
                 variable.set('Invalid file')
             for frame in self._csd_info_frames:
-                frame.update_data_labels(None)
+                frame.update_csd_info()
         else:
             for attribute, variable in self._file_info.items():
                 variable.set(getattr(file, attribute))
             csd = file.csd
             for frame in self._csd_info_frames:
-                frame.update_data_labels(csd)
+                frame.update_csd_info(csd)
