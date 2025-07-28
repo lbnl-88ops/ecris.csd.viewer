8ea6bed2e87b1a2985dd7b7e46e686d08310eb5a
Jessica Rehak <rehak@lbl.gov>
2025-07-28 13:11:22 -0700

    updated version to 1.2.0-beta.3

--- ecris/csd/viewer/app.py
+++ ecris/csd/viewer/app.py
@@ -22,7 +22,7 @@ from ecris.csd.viewer.gui.style.patchMatplotlib import applyPatch
 from .gui import FileList, PlotControls, Plot, FileListControls, AppMenu, DiagnosticWindow, FileInfoPane
 
 
-__version__ = "1.2.0-beta.2"
+__version__ = "1.2.0-beta.3"
 
 matplotlib.rc('font', size=14)
 applyPatch()
