2c3dd215562dc7a136428436f9cd1a6d1d6c478b
Jessica Rehak <rehak@lbl.gov>
2025-09-26 15:58:26 -0700

    populate listbox updated to fix memory leak

--- csd_viewer/gui/controls/file_list.py
+++ csd_viewer/gui/controls/file_list.py
@@ -62,16 +62,21 @@ class FileList(tk.Frame):
                                                 selectforeground='white')
 
     def populate_listbox(self, retain_plotted=False):
-        """Populates the listbox with files from the specified directory."""
-        plotted = []
+        old_plotted_files = {}
         if retain_plotted:
-            plotted = [f.path for f in self.files if f.plotted]
-        self.files = get_files(self.current_directory)
+            old_plotted_files = {f.path: f for f in self.files if f.plotted}
 
-        for plotted_file in plotted:
-            for i, file in enumerate(self.files):
-                if file == plotted_file:
-                    self.files[i] = plotted_file
+        current_files_on_disk = get_files(self.current_directory)
+        
+        new_file_list = []
+        for file_obj in current_files_on_disk:
+            if file_obj.path in old_plotted_files:
+                new_file_list.append(old_plotted_files[file_obj.path])
+            else:
+                new_file_list.append(file_obj)
+        
+        self.files = new_file_list
+
         self.file_listbox.delete(0, tk.END)
         if not self.files:
             self.stringvar.set(["No CSD files found"])
@@ -79,7 +84,5 @@ class FileList(tk.Frame):
         else:
             self.stringvar.set([f.list_value for f in self.files])
             self.file_listbox.configure(state=tk.NORMAL)
-        for file in self.files:
-            file.plotted = file.path in plotted
-        self.update_colors()
         
+        self.update_colors()
